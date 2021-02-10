6d1b784ac551182f63f47ef6fd0c572679854001
aebagnasco <78452069+aebagnasco@users.noreply.github.com>
2021-02-09 20:22:01 -0800

    changed icon id from iconmain to icon for simplicity

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function displayTemp(response) {
     response.data.wind.speed
   );
   document.querySelector("#condition").innerHTML = response.data.weather[0].main;
-  let iconElement = document.querySelector("#iconmain");
+  let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src", 
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
