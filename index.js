26a3a2c2e98202f8bee0e938b894ae01c3132136
aebagnasco <78452069+aebagnasco@users.noreply.github.com>
2021-02-10 15:22:39 -0800

    added active class link

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ function displayTemp(response) {
   iconElement.setAttribute(
     "src", 
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    fahrenheitTemperature = response.data.main.temp;
   // can console log response.data to find where ^ humidity, wind, temp is located
 }
 
@@ -41,6 +42,7 @@ function handleSubmit(event) {
   let apiKey = "216598f817e9e34d672a7a0c88bebdfa";
   // ^ a string 
   let city = document.querySelector("#city-input").value;
+  // this essentially logs the city that was typed into the search form but below, using the api, we will get the url to that city and its appropriate info about that city 
   let units = "imperial";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   // can console log 'apiUrl' at this time to make sure it is connected
@@ -51,6 +53,7 @@ function handleSubmit(event) {
 }
 
 let form = document.querySelector("#search-form");
+// this takes control over the form and changes the control to js and not html 
 form.addEventListener("submit", handleSubmit);
 
 function showPosition(position) {
@@ -64,11 +67,41 @@ function showPosition(position) {
 function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(showPosition);
 }
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temp");
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let celsiusTemperature = (fahrenheitTemperature - 32) * 5/9;
+
+  temperature.innerHTML = Math.round(celsiusTemperature);
+
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
+  let temperature = document.querySelector("#temp");
+
+  temperature.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+let fahrenheitTemperature = null;
+
 let button = document.querySelector("#geoweather");
 button.addEventListener("click", getCurrentPosition);
+// ^these are global variables, not local variables, meaning they are operating outside a function and are accessible from inside functions 
 
 // whenever i click on the button, i want to display the current position
 
 // summary (few patterns/tricks)
 // always add an id to an element i want to interact with
 // select it and then handle the callbacks 
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
