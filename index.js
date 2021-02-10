830aa4e4adb137787470a37ef40d7f24b6d57943
aebagnasco <78452069+aebagnasco@users.noreply.github.com>
2021-02-09 20:19:41 -0800

    made icon function work by adding location of data from API string

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function displayTemp(response) {
   let iconElement = document.querySelector("#iconmain");
   iconElement.setAttribute(
     "src", 
-    `http://openweathermap.org/img/wn/04d@2x.png`);
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   // can console log response.data to find where ^ humidity, wind, temp is located
 }
 
